34f8635dd3cdfd2372b3a2a23e6757438aee109e
Marcus Gustafsson <marcusgu@hotmail.com>
2017-12-30 21:28:09 +0100

    test

---
+++ src/oldindextest.js
@@ -0,0 +1,12 @@
+// const request = require('supertest');
+//
+// import Index from './index';
+//
+// describe('Test the '/' path', () => {
+//     test('It should respond with the GET method', (done) => {
+//         request(Index).get('/').then((response) => {
+//             expect(response.statusCode).toBe(200);
+//             done();
+//         });
+//     });
+// });
