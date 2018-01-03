9688aa62b53cfcac3818455f087efb12672c6c5d
Marcus Gustafsson <marcusgu@hotmail.com>
2018-01-03 08:28:53 +0100

    test

--- src/components/admin.test.js
+++ src/components/admin.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 // import ReactDOM from 'react-dom';
-import Admin from './Admin';
+import Admin from './admin';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
