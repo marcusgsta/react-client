eed348ff8278e2ccddbff3ee3c28524e13b8aa6c
Marcus Gustafsson <marcusgu@hotmail.com>
2018-01-07 09:59:29 +0100

    about etc

--- src/components/about.js
+++ src/components/about.js
@@ -1,15 +1,58 @@
 import React from 'react';
-import expressNode from '../img/express.jpg';
+import expressLogo from '../img/express_icon.png';
+import reactLogo from '../img/react_logo.svg';
+import socketIo from '../img/socket.io.png';
+import jwtLogo from '../img/jwt.png';
+const FontAwesome = require('react-fontawesome');
 
 const About = () => (
     <div className="about-page">
-        <h2>Om</h2>
-        <p>Den här sidan har gjorts med hjälp av Express.js,
-             vilket skapar en server för back-end tillsammans
-             med React.js som sköter om front-end.</p>
-        <img id="express" alt="Express and Node" src={expressNode}/>
-        <p>Koden finns på Github</p>
-        <a href="https://github.com/marcusgsta/ramverk2-app">Ramverk2-app</a>
+        <h2>Om React Chat</h2>
+        <p>Jag som kodat React Chat heter Marcus Gustafsson. Appen är slutprojektet för kursen Ramverk2, i utbildningen Webbprogrammering vid Blekinge Tekniska Högskola.</p>
+
+        <div className="wrap">
+            <section>
+                <h3>Express.js</h3>
+                <img alt="Express and Node" src={expressLogo}/>
+                <p>Servern startas av <a href="https://expressjs.com/">Express.js</a>.</p>
+            </section>
+
+            <section>
+                <img id="reactLogo" alt="React Logo" src={reactLogo}/>
+                <h3>React.js</h3>
+
+                <p>Användargränssnittet är gjort i <a href="https://reactjs.org/">React.js</a>.</p>
+            </section>
+
+            <section>
+                <h3>socket.io</h3>
+                <img id="socketio" alt="Socket.io Logo" src={socketIo}/>
+                <p>Realtidschatten använder <a href="https://socket.io/">socket.io</a>.</p>
+            </section>
+
+            <section>
+                <h3>JWT</h3>
+                <img id="jwt" alt="JWT Logo" src={jwtLogo}/>
+                <p><a href="https://jwt.io/">JSON Web Token</a> tar hand om autentificering och inloggning av användare och administratör.</p>
+            </section>
+
+        </div>
+
+        <p>Koden finns på Github. Där finns också instruktioner för hur man kan ladda ner och köra projektet på sin egen dator.</p>
+
+        <div className="githubLink">
+            <a href="https://github.com/marcusgsta/ramverk2-app">Server</a>
+            <FontAwesome
+                name='fab fa-github'
+                size='2x'/>
+        </div>
+
+        <div className="githubLink">
+            <a href="https://github.com/marcusgsta/react-client">Klient</a>
+            <FontAwesome
+                name='fab fa-github'
+                size='2x'/>
+        </div>
     </div>
 );
 
--- src/components/admin.js
+++ src/components/admin.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+const FontAwesome = require('react-fontawesome');
 
 const Admin = () => (
     <div className="admin">
         <h2>Admin</h2>
-        <li><Link to="/update">ikon Uppdatera</Link></li>
-        <li><Link to="/remove">ikon Radera</Link></li>
+        <div>
+            <div className="adminLink">
+                <Link to="/update">Uppdatera</Link>
+                <FontAwesome
+                    name='fas fa-edit'
+                    size='2x'/>
+            </div>
+            <div className="adminLink">
+                <Link to="/remove">Radera</Link>
+                <FontAwesome
+                    name='fas fa-trash'
+                    size='2x'/>
+            </div>
+        </div>
     </div>
 );
 
